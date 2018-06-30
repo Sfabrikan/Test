38f2bb08836d4529a922ac0572729de88fd18edc
Sfabrikan <sfabrikan.ini@gmail.com>
2018-06-30 18:06:29 +1000

    1

---
+++ script/script.lua
@@ -0,0 +1,7 @@
+--local xml2lua = require("C:\\Users\\sfab\\Documents\\GitHub\\Test\\libs\\xml2lua-master\\xml2lua");
+--local handler = require("C:\\Users\\sfab\\Documents\\GitHub\\Test\\libs\\xml2lua-master\\xml2lua\\xmlhandler.tree");
+
+--t = xml2lua.parser(handler)
+--parser:parse(xml)
+
+print(LUA_PATH)
